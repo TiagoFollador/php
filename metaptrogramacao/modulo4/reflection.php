a61ba32b3e4d90102b997e207cc53b857c89b3bb
estagiario-dev2 <estagiario-dev2@escape79.com.br>
2025-01-09 08:53:54 -0300

    Accessing private properties

---
+++ metaptrogramacao/modulo4/reflection.php
@@ -0,0 +1,46 @@
+<?php
+
+use Alura\Reflection\ClasseExemplo;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$reflectionClass = new ReflectionClass(ClasseExemplo::class);
+
+$objetoClasseExemplo = $reflectionClass->newInstanceWithOutConstructor();
+$propriedadePrivada = $reflectionClass->getProperty('propriedadePrivada');
+var_dump($propriedadePrivada->getValue($objetoClasseExemplo));
+
+if (!$propriedadePrivada->isPublic()) {
+    $propriedadePrivada->setAccessible(true);
+}
+
+var_dump($propriedadePrivada->getValue($objetoClasseExemplo));
+
+// ------------------------------------------- Métodos ------------------------------------------
+
+$reflectionMethod = $reflectionClass->getMethod('metodoProtegido');
+$reflectionMethod->setAccessible(true);
+var_dump($reflectionMethod->invoke($objetoClasseExemplo));
+
+/*
+ 
+ 
+ 
+ 
+ $parameters = array_filter(
+    $reflectionMethod->getParameters(), 
+    fn (ReflectionParameter $parameter) => !$parameter->isOptional()
+ );
+ foreach ($parameters as $parameter) {
+    if (!$parameter->hasType()) {
+        throw new DomainException('Nao tem tipo :(');
+    }
+    
+    $tipo = (string) $parameter->getType();
+    var_dump($tipo, $parameter->getType()->isBuiltin());
+ }
+
+
+ $reflectionMethod->invokeArgs($objetoClasseExemplo, ["Ante diegmon", 42]);
+ 
+ */
